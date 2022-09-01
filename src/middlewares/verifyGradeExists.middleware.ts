017f533d29f1b1750056167cdc3f5b5602635b45
Gabriel <gabriel.such@hotmail.com>
2022-09-01 14:57:59 -0300

    feat: created Grade Exists Middleware

---
+++ src/middlewares/verifyGradeExists.middleware.ts
@@ -0,0 +1,19 @@
+import {Request, Response, NextFunction} from "express"
+
+import {AppDataSource} from "../data-source"
+import {Grade} from "../entities/grade.entity"
+
+
+const verifyGradeExistsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const gradeRepository = AppDataSource.getRepository(Grade)
+    const gradeExists = await gradeRepository.findOneBy({
+        id: req.params.grade_id
+    }) 
+
+    if(!gradeExists) {
+        return res.status(404).json({error: "Grade not found."})
+    }
+}
+
+
+export default verifyGradeExistsMiddleware
