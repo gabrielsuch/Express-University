85d5f7ac4afbc4e783df4691a8188ef29053452b
Gabriel <gabriel.such@hotmail.com>
2022-09-19 13:46:34 -0300

    feat: commented an example

--- src/routes/grade.routes.ts
+++ src/routes/grade.routes.ts
@@ -5,6 +5,7 @@ import GradeController from "../controllers/grade.controller"
 import verifyTokenMiddleware from "../middlewares/verifyToken.middleware"
 import verifyAdminPermissionMiddleware from "../middlewares/verifyAdminPermission.middleware"
 import verifyGradeExistsMiddleware from "../middlewares/verifyGradeExists.middleware"
+import verifyCourseExistsMiddleware from "../middlewares/verifyCourseExists.middleware"
 
 
 const route = Router()
@@ -14,6 +15,7 @@ const gradeRoute = () => {
     route.get("/:grade_id", verifyGradeExistsMiddleware, GradeController.getGrade)
     route.get("", GradeController.getGrades)
     route.post("", verifyTokenMiddleware, verifyAdminPermissionMiddleware, GradeController.createGrade)
+    route.post("/assign/:grade_id/:course_id", verifyTokenMiddleware, verifyAdminPermissionMiddleware, verifyGradeExistsMiddleware, verifyCourseExistsMiddleware, GradeController.assignGradeToCourse)
     route.patch("/:grade_id", verifyTokenMiddleware, verifyGradeExistsMiddleware, verifyAdminPermissionMiddleware, GradeController.updateGrade)
     route.delete("/:grade_id", verifyTokenMiddleware, verifyGradeExistsMiddleware, verifyAdminPermissionMiddleware, GradeController.deleteGrade)
 
