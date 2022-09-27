808d7d80b2d2cdc954cddf3b19f69b33911dffc1
Gabriel <gabriel.such@hotmail.com>
2022-09-27 15:32:19 -0300

    feat: added validate UUID middleware on Route

--- src/middlewares/validateUUID.middleware.ts
+++ src/middlewares/validateUUID.middleware.ts
@@ -1,10 +1,16 @@
 import {Request, Response, NextFunction} from "express"
 
-import {} from "uuid"
+import {validate, version} from "uuid"
 
 
 const validateUUIDMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const validateUUID = Object.keys(req.params).map((key) => {
+        return validate(req.params[key]) && version(req.params[key]) === 4
+    })
 
+    if(validateUUID.includes(false)) {
+        return res.status(400).json({error: "Not a UUID type."})
+    }
 
     return next()
 }
--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -5,6 +5,7 @@ import EmployeeController from "../controllers/employee.controller"
 import verifyTokenMiddleware from "../middlewares/verifyToken.middleware"
 import verifyAdminPermissionMiddleware from "../middlewares/verifyAdminPermission.middleware"
 import validateSchemaMiddleware from "../middlewares/validateSchema.middleware"
+import validateUUIDMiddleware from "../middlewares/validateUUID.middleware"
 
 import {createEmploeeSchema, updateEmploeeSchema, loginEmploeeSchema} from "../schemas/employee.schema"
 
@@ -14,14 +15,14 @@ const route = Router()
 
 const employeeRoute = () => {
     route.get("/current", verifyTokenMiddleware, EmployeeController.getCurrentEmployee)
-    route.get("/:id", verifyTokenMiddleware, EmployeeController.getEmployee)
+    route.get("/:id", validateUUIDMiddleware, verifyTokenMiddleware, EmployeeController.getEmployee)
     route.get("", verifyTokenMiddleware, verifyAdminPermissionMiddleware, EmployeeController.getAllEmployees)
     
     // route.post("", verifyTokenMiddleware, verifyAdminPermissionMiddleware, EmployeeController.createEmployee)
     route.post("", validateSchemaMiddleware(createEmploeeSchema), EmployeeController.createEmployee)
     route.post("/login", validateSchemaMiddleware(loginEmploeeSchema), EmployeeController.login)
-    route.patch("/:id", verifyTokenMiddleware, validateSchemaMiddleware(updateEmploeeSchema), verifyAdminPermissionMiddleware, EmployeeController.updateEmployee)
-    route.delete("/:id", verifyTokenMiddleware, verifyAdminPermissionMiddleware, EmployeeController.deleteEmployee)
+    route.patch("/:id", validateUUIDMiddleware, verifyTokenMiddleware, validateSchemaMiddleware(updateEmploeeSchema), verifyAdminPermissionMiddleware, EmployeeController.updateEmployee)
+    route.delete("/:id", validateUUIDMiddleware, verifyTokenMiddleware, verifyAdminPermissionMiddleware, EmployeeController.deleteEmployee)
 
     return route
 }
