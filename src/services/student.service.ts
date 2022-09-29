9b2deafca07a8f231f53da81a5110997f9207bbe
Gabriel <gabriel.such@hotmail.com>
2022-09-29 19:14:59 -0300

    fix: replaced User to Student

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -109,7 +109,7 @@ class StudentSerivce {
         return {status: 200, message: updatedStudent}
     }
 
-    deleteStudent = async ({decoded}: Request) => {
+    deleteCurrentStudent = async ({decoded}: Request) => {
         const studentRepository = AppDataSource.getRepository(Student)
         const currentStudent = await studentRepository.findOneBy({
             email: decoded
