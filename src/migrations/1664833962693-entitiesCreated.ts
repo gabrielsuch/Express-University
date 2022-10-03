665fa779feb92be5109ec8a18d02b836f231d7d5
Gabriel <gabriel.such@hotmail.com>
2022-10-03 18:54:55 -0300

    feat: created new migration and inserted default on field

--- src/migrations/1664833962693-entitiesCreated.ts
+++ src/migrations/1664833962693-entitiesCreated.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class entitiesCreated1664478195865 implements MigrationInterface {
-    name = 'entitiesCreated1664478195865'
+export class entitiesCreated1664833962693 implements MigrationInterface {
+    name = 'entitiesCreated1664833962693'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "rating" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying(255) NOT NULL, "courseId" uuid, "studentId" uuid, CONSTRAINT "PK_ecda8ad32645327e4765b43649e" PRIMARY KEY ("id"))`);
@@ -11,7 +11,7 @@ export class entitiesCreated1664478195865 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "status_course" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "status" "public"."status_course_status_enum" NOT NULL DEFAULT 'Incompleto', "duration" double precision NOT NULL, "coursesId" uuid, "studentId" uuid, CONSTRAINT "PK_beaffffc280da5e04d03637a0ba" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "student" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(100) NOT NULL, "birthdate" TIMESTAMP NOT NULL, "cpf" character varying(14) NOT NULL, "telephone" character varying(14), "cellphone" character varying(15), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "sex" "public"."student_sex_enum" NOT NULL DEFAULT 'null', "email" character varying(150) NOT NULL, "password" character varying(255) NOT NULL, "courseId" uuid, CONSTRAINT "UQ_79dfe04f9559f2e68e849df789c" UNIQUE ("cpf"), CONSTRAINT "UQ_a56c051c91dbe1068ad683f536e" UNIQUE ("email"), CONSTRAINT "PK_3d8016e1cb58429474a3c041904" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "type_course" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(100) NOT NULL, CONSTRAINT "UQ_42ef5beec926f688bb960cb5479" UNIQUE ("name"), CONSTRAINT "PK_048a1055a819e5a0c07dc60ec11" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "course" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(150) NOT NULL, "duration" double precision NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "typeId" uuid, CONSTRAINT "UQ_30d559218724a6d6e0cc4f26b0e" UNIQUE ("name"), CONSTRAINT "PK_bf95180dd756fd204fb01ce4916" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "course" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(150) NOT NULL, "duration" double precision NOT NULL DEFAULT '0', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "typeId" uuid, CONSTRAINT "UQ_30d559218724a6d6e0cc4f26b0e" UNIQUE ("name"), CONSTRAINT "PK_bf95180dd756fd204fb01ce4916" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "rating" ADD CONSTRAINT "FK_1283cbb80fa7bddb804f81fa10d" FOREIGN KEY ("courseId") REFERENCES "course"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "rating" ADD CONSTRAINT "FK_b110361349c7bcee2966f39ef5a" FOREIGN KEY ("studentId") REFERENCES "student"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "grade" ADD CONSTRAINT "FK_1b8c18efb3f2ecaf5603e2729ee" FOREIGN KEY ("courseId") REFERENCES "course"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
--- src/schemas/course.schema.ts
+++ src/schemas/course.schema.ts
@@ -3,12 +3,10 @@ import * as yup from "yup"
 
 const createCourseSchema = yup.object().shape({
     name: yup.string().required().max(150),
-    duration: yup.number().required().positive()
 })
 
 const updateCourseSchema = yup.object().shape({
     name: yup.string().optional().max(150),
-    duration: yup.number().optional().positive()
 })
 
 
